4fe29e5131e241620224c72f8e6c5f93b2e7956c
AlejandroAP98 <aalvarezpa@unal.edu.co>
2025-03-13 13:49:30 -0500

    se crea archivo con variables de entorno

--- src/components/Services/API.ts
+++ src/components/Services/API.ts
@@ -1,12 +1,12 @@
-export const API_BASE_URL = 'http://localhost:3000/api';
-export const LOGIN = '/users/login';
-export const PRODUCTOS = '/products';
-export const CATEGORIAS = '/categories';
-export const MARCAS = '/brands';
-export const USUARIOS = '/users';
-export const SALAS = '/salas';
-export const ROLES = '/roles'
-export const MOVIMIENTOS = '/movimientos';
-export const SALAS_PRODUCTOS = '/salas_productos';
-export const REPORTE = '/reporte';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const LOGIN = import.meta.env.VITE_LOGIN;
+export const PRODUCTOS = import.meta.env.VITE_PRODUCTOS;
+export const CATEGORIAS = import.meta.env.VITE_CATEGORIAS;
+export const MARCAS = import.meta.env.VITE_MARCAS;
+export const USUARIOS = import.meta.env.VITE_USUARIOS;
+export const SALAS = import.meta.env.VITE_SALAS;
+export const ROLES = import.meta.env.VITE_ROLES;
+export const MOVIMIENTOS = import.meta.env.VITE_MOVIMIENTOS;
+export const SALAS_PRODUCTOS = import.meta.env.VITE_SALAS_PRODUCTOS;
+export const REPORTE = import.meta.env.VITE_REPORTE;
 
